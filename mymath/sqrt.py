56c7e12980b4129ccc0c97cfa2dbb5b923a9cb3c
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-20 18:56:58 +0500

    Update sqrt.py

--- mymath/sqrt.py
+++ mymath/sqrt.py
@@ -1,1 +1,18 @@
+from math import factorial
 
+def sqrt(x: float, n_terms: int = 10) -> float:
+    """
+Calculates sqrt(x) using the Taylor expansion around a=1.
+n_terms is the number of terms in the series.
+    """
+    if x <= 0:
+        raise ValueError("x must be positive")
+    y = x - 1
+    result = 0
+    for n in range(n_terms):
+        coeff = 1
+        for k in range(n):
+            coeff *= (0.5 - k)
+        coeff /= factorial(n)
+        result += coeff * (y ** n)
+    return result
