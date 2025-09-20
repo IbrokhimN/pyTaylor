053c2e2e5338b5692ce2069d70607b0969b3201f
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-20 18:49:25 +0500

    Update exp.py

--- mymath/exp.py
+++ mymath/exp.py
@@ -1,1 +1,15 @@
+# exp realization with usage of Taylor series
+# Ik that i could do just e**x but i try to realize almost everything with taylor series
 
+
+def exp(x: float) -> float:
+    """
+    Calculate e^x using Taylor series approximation.
+    
+    Parameters:
+        x (float): exponent
+        
+    Returns:
+        float: approximate value of e^x
+    """
+    return ( 1 + x + ((x**2)/2) + ((x**3)/6) + ((x**4)/24) ) # Taylor series formula for exp
