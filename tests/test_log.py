41a2840c53931930167e8750796ae1e7b70d9e84
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-20 19:10:59 +0500

    Update test_log.py

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,1 +1,19 @@
+import math
+import sys, os
+import matplotlib.pyplot as plt
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from mymath.log import ln
+vals = [i/10 for i in range(1, 101)] 
+math_vals = [math.log(x) for x in vals]
+my_vals = [ln(x) for x in vals]
+plt.figure(figsize=(12,6))
+plt.plot(vals, math_vals, label='math.log', color='blue')
+plt.plot(vals, my_vals, label='mymath.ln', color='red', linestyle='--')
+plt.title("Сравнение math.log и mymath.ln")
+plt.xlabel("x")
+plt.ylabel("ln(x)")
+plt.legend()
+plt.grid(True)
+plt.savefig("log_comparison.png", dpi=300)
+plt.show()
