bf96e59041c7117e7ad74bed52219bf5c7493085
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2025-09-20 18:32:16 +0500

    Update cos.py

--- mymath/cos.py
+++ mymath/cos.py
@@ -1,1 +1,15 @@
+# cosine realization with usage of Taylor series
+
+'''
+input should be the float in grad
+example: x = 50°
+and by using Taylor series formula the cos of x will be calculated 
+more in README.md file
+'''
+
+from mymath.utils import PI, checking
+
+def cos(x: float) -> float:
+    x = checking(x)
+    return ( 1 - ((x**2)/2) + ((x**4)/24) - ((x**6)/720) +((x**8)/40320) ) # Taylor series formula for cos
 
